31f0252d95fc29a92faa88ed693cde0c43cfba01
OmarYeh <119100020+OmarYeh@users.noreply.github.com>
2025-06-03 13:54:01 +0300

    fixing companyid screen in onboardingstack

--- Screens/PickVoice.tsx
+++ Screens/PickVoice.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import {View, Image, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import {useNavigation,useRoute} from '@react-navigation/native';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 const PickVoice = () => {
   const navigation = useNavigation();
     const route = useRoute();
@@ -9,8 +9,9 @@ const PickVoice = () => {
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <TouchableOpacity
         style={{padding: 10, backgroundColor: 'blue', borderRadius: 5}}
-        onPress={() => {
-         navigation.replace('MainScreen');
+        onPress={async () => {
+          await AsyncStorage.setItem('onboardingDone', 'true');
+         navigation.replace('HomeStackScreen');
         }}>
         <Text style={{color: 'white', fontSize: 16}}>Dismiss</Text>
       </TouchableOpacity>
